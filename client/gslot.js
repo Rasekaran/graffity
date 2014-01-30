8d54bacc32225e058e33aa184d16a16fdd974560
rasekaran <rasikaran1984@gmail.com>
2014-01-30 17:05:14 +0530

    bug fixes

--- client/gslot.js
+++ client/gslot.js
@@ -2,11 +2,11 @@ Template.gslot.helpers({
     isReserved: function() {
         
         return this.status == "reserved";
-    }
+    },
     isUploaded: function() {
         
         return this.status == "uploaded";
-    }
+    },
     isOwnSlot: function(){
     	return this.ownerId == Meteor.user()._id;
     }
